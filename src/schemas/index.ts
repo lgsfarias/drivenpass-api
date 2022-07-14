b7866bde3a4cf547933dcb289829a45e03fe9677
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-07-14 03:44:46 -0300

    feat: card schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,5 +2,6 @@ import signin from './signin.schema.js';
 import signup from './signup.schema.js';
 import credential from './credential.schema.js';
 import secureNote from './secureNote.schema.js';
+import card from './card.schema.js';
 
-export { signin, signup, credential, secureNote };
+export { signin, signup, credential, secureNote, card };
