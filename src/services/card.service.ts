da8840dd30f3d5d501820b6b99f8319d3671adcc
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-07-14 03:41:14 -0300

    feat: card service

---
+++ src/services/card.service.ts
@@ -0,0 +1,35 @@
+import AppError from '../utils/AppError.js';
+import * as cardRepository from '../repositories/card.repository.js';
+
+export const create = async (createCardData: cardRepository.CreateCardData) => {
+  const card = await cardRepository.create(createCardData);
+  return card;
+};
+
+export const findAllByUserId = async (userId: number) => {
+  const cards = await cardRepository.findAllByUserId(userId);
+  return cards;
+};
+
+export const findById = async (id: number) => {
+  const card = await cardRepository.findById(id);
+  if (!card) {
+    throw new AppError('Secure note not found', 404);
+  }
+  return card;
+};
+
+export const verifyIfLabelAlreadyExists = async (
+  label: string,
+  userId: number,
+) => {
+  const card = await cardRepository.findByLabelAndUserId(label, userId);
+  if (card) {
+    throw new AppError('Label already exists', 400);
+  }
+};
+
+export const deletecard = async (id: number) => {
+  const card = await cardRepository.deletecard(id);
+  return card;
+};
