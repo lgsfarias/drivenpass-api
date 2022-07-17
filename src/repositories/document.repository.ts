57f84df78b935901aa38b0ef4af69b8a017c7a5e
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-07-16 23:25:27 -0300

    feat: create document repository

---
+++ src/repositories/document.repository.ts
@@ -0,0 +1,51 @@
+import { Document, DocumentType } from '@prisma/client';
+import prisma from '../config/database.js';
+
+export type CreateDocumentData = Omit<Document, 'id' | 'createdAt'>;
+
+export const create = async (createDocumentData: CreateDocumentData) => {
+  const document = await prisma.document.create({
+    data: createDocumentData,
+  });
+  return document;
+};
+
+export const findByLabelAndUserId = async (
+  type: DocumentType,
+  userId: number,
+) => {
+  const document = await prisma.document.findFirst({
+    where: {
+      type,
+      userId,
+    },
+  });
+  return document;
+};
+
+export const findAllByUserId = async (userId: number) => {
+  const documents = await prisma.document.findMany({
+    where: {
+      userId,
+    },
+  });
+  return documents;
+};
+
+export const findById = async (id: number) => {
+  const document = await prisma.document.findUnique({
+    where: {
+      id,
+    },
+  });
+  return document;
+};
+
+export const deleteDocument = async (id: number) => {
+  const document = await prisma.document.delete({
+    where: {
+      id,
+    },
+  });
+  return document;
+};
